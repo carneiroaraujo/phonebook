56d53eb299fc6d84e83905ceb6e52ca69093a3e1
carneiroaraujo <joaosheep95@gmail.com>
2024-04-30 15:37:02 -0300

    Exercises 3.7.-3.8.

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-const express = require('express')
+const express = require('express') 
+const morgan = require('morgan')
+
 const app = express()
 app.use(express.json())
+app.use(morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    JSON.stringify(req.body)
+    
+  ].join(' ')
+}))
 const PORT = 3001
 
 let entries = [
@@ -71,8 +84,11 @@ app.post("/api/persons", (request, response)=>{
   console.log("ok");
   response.status(204).end()
 })
+function unknownEnpoint(request, response, next) {
+  response.status(404).send({error:"unknown endpoint"})
+}
 
-
+app.use(unknownEnpoint)
 
 app.listen(PORT, ()=>{
     console.log(`Listening on port ${PORT}`);
